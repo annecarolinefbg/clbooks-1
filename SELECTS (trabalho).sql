32dd4a3731d55d8e0be0b6d70910c6df2f8d4865
carol <159432484+qrsluiza@users.noreply.github.com>
2024-11-07 09:56:26 -0300

    Create SELECTS (trabalho).sql

---
+++ SELECTS (trabalho).sql
@@ -0,0 +1,14 @@
+#Tela fórum:
+
+#Select para mostrar as postagens mais curtidas
+
+#Select para filtrar apenas as postagens com fotos
+
+#select para filtrar as postagens do usuário (usuário aqui)
+
+
+#Tela perfil usuario e escritor:
+
+#Select para mostrar os livros publicados pelo usuários do clbooks
+
+#Select para filtar os escritores que mais escreveram romances
