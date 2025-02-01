d49155e317c0e5ffe63cf7b5ec6d0efc1cfefe45
annecarolinefbg <accp1@aluno.ifal.edu.br>
2025-02-01 19:33:11 -0300

    update

--- 4o-bimestre/sequelize-0/db.js
+++ 4o-bimestre/sequelize-0/db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(
-    'wmtunes', 
+    'clbooks', 
     'aluno.ifal', // nome de usuario  
     'aluno.ifal', // senha de acesso
     {
